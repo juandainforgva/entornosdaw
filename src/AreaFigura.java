dffd335ed239c39cda424e69bdc088baa798473a
David <davcerval@alu.edu.gva.es>
2024-12-05 16:04:43 +0100

    errores corregidos

--- src/AreaFigura.java
+++ src/AreaFigura.java
@@ -8,24 +8,25 @@ public class AreaFigura {
         System.out.println("1. Calcular área de un rectángulo");
         System.out.println("2. Calcular área de un círculo");
         System.out.println("Ingrese su opción:");
-        int opcion = scanner.nextLine();
+        int opcion = scanner.nextInt();
 
-        if (opción == 1) {
+        if (opcion == 1) {
             System.out.println("Ingrese la base del rectángulo: ");
-            double base = scanner.nextInt(); // Error: Método incorrecto para leer decimales
+            double base = scanner.nextDouble(); // Corregido especificando que lea un double.
             System.out.println("Ingrese la altura del rectángulo: ");
-            double altura = scanner.next();
+            double altura = scanner.nextDouble();
 
             double area = base * altura;
-            System.out.println("El área del rectángulo es " + area); // Error: Error al formatear salida
-        } else if (opcion = 2) { // Error: Asignación en lugar de comparación
+            System.out.println("El área del rectángulo es " + area); 
+        } else if (opcion == 2) { // Corregido con el símbolo correcto
             System.out.println("Ingrese el radio del círculo: ");
-            double radio = scanner.nextInt(); // Error: Método incorrecto para leer decimales
+            double radio = scanner.nextDouble(); // Corregido especificando que lea un double.
 
-            double area = 3.1416 * radio ^ 2;
+            double area = 3.1416 * Math.pow(radio, 2);
             System.out.println("El área del círculo es: " + area);
         } else {
-            System.out.println("Opción inválida!"); // Error: No se cierra el scanner
+            System.out.println("Opción inválida!");
+            scanner.close(); //Scanner cerrado.
         }
     }
 }
